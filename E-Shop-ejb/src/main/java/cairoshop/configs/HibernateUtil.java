9a47a7ea1d9d0f5a4456e47eaa7034349c21955d
muhamed-hassan <mohamed.hassan.work@gmail.com>
2018-03-17 19:46:11 +0200

    code refactoring

--- E-Shop-ejb/src/main/java/cairoshop/configs/HibernateUtil.java
+++ E-Shop-ejb/src/main/java/cairoshop/configs/HibernateUtil.java
@@ -65,7 +65,9 @@ public class HibernateUtil {
                 .map(p -> {
                     try {
                         String currentFileName = p.toFile().getName();
-                        return Class.forName(packageName + '.' + currentFileName.substring(0, currentFileName.length() - 6));
+                        return Class.forName(new StringBuilder()
+                                .append(packageName).append(".")
+                                .append(currentFileName.substring(0, currentFileName.length() - 6)).toString());
                     } catch (ClassNotFoundException ex) {
                         throw new RuntimeException("Failed in reading .class file of " + ex);
                     }
