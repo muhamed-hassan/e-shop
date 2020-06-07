cbaf7e13d5070c08ba533eff876b2567764835ec
muhamed_qotb <mohamed.hassan.work@gmail.com>
2020-06-07 10:42:48 +0200

    adding flyway for db migration

--- E-Shop-ejb/src/main/java/cairoshop/configs/HibernateConfigurator.java
+++ E-Shop-ejb/src/main/java/cairoshop/configs/HibernateConfigurator.java
@@ -1,17 +1,23 @@
 package cairoshop.configs;
 
-import cairoshop.configs.utils.ConfigUtil;
-import com.demo.GlobalLogger;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import javax.sql.DataSource;
+import javax.annotation.Resource;
+
 import org.apache.logging.log4j.Level;
+import org.flywaydb.core.Flyway;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.demo.GlobalLogger;
+
+import cairoshop.configs.utils.ConfigUtil;
 
 /* ************************************************************************** 
  * Developed by: Muhamed Hassan	                                            *
@@ -28,36 +34,41 @@ public class HibernateConfigurator {
     private SessionFactory sessionFactory;
 
     private StandardServiceRegistry standardServiceRegistry;
-    
+
     @Inject
     private GlobalLogger globalLogger;
-    
+
+    @Resource(name = "java:global/jdbc/shopDS")
+    private DataSource dataSource;
+
     @PostConstruct
     public void init() {
         try {
-            
-            standardServiceRegistry
-                    = new StandardServiceRegistryBuilder()
-                    .configure("hibernate.cfg.xml")
-                    .build();
+            standardServiceRegistry = new StandardServiceRegistryBuilder()
+                                            .configure("hibernate.cfg.xml")
+                                            .build();
 
-            MetadataSources sources = new MetadataSources(standardServiceRegistry);
-            configUtil
-                    .getClasses("cairoshop.entities")
-                    .forEach(entity -> sources.addAnnotatedClass(entity));
+            MetadataSources sources = new MetadataSources(standardServiceRegistry)
+                                            .addPackage("cairoshop.entities");
+            /*configUtil.getClasses("cairoshop.entities")
+                        .forEach(entity -> sources.addAnnotatedClass(entity));*/
 
-            sessionFactory = sources
-                    .getMetadataBuilder()
-                    .build()
-                    .getSessionFactoryBuilder()
-                    .build();
+            sessionFactory = sources.getMetadataBuilder()
+                                        .build()
+                                        .getSessionFactoryBuilder()
+                                        .build();
 
+            Flyway flyway = new Flyway();
+            flyway.setSchemas("cairoshop");
+            flyway.setLocations("db");
+            flyway.setDataSource(dataSource);
+            flyway.migrate();
         } catch (Exception ex) {
             globalLogger
                     .doLogging(
-                            Level.FATAL, 
-                            "An error occured during session factory initialization", 
-                            getClass(), 
+                            Level.FATAL,
+                            "An error occured during session factory initialization",
+                            getClass(),
                             ex
                     );
         }
