63f804bc1ac09936e57a3e7f44b9293b3cc698b8
muhamed_qotb <mohamed.hassan.work@gmail.com>
2020-06-14 14:24:33 +0200

    adding jersey and it's configs

--- E-Shop-web/src/main/java/cairoshop/configs/JaxRsConfig.java
+++ E-Shop-web/src/main/java/cairoshop/configs/JaxRsConfig.java
@@ -8,5 +8,5 @@ import javax.ws.rs.core.Application;
  * LinkedIn    : https://www.linkedin.com/in/mohamed-qotb/                  *  
  * GitHub      : https://github.com/muhamed-hassan                          *  
  * ************************************************************************ */
-@ApplicationPath("ws-api")
-public class JaxRsConfig extends Application {}
+//@ApplicationPath("ws-api")
+public class JaxRsConfig /*extends Application*/ {}
